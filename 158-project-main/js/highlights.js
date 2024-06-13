9c975d7ab521f1eb8382f2b628ab0fd8f36b654f
sriya-sn <81259869+sriya-sn@users.noreply.github.com>
2024-06-12 20:55:51 -0400

    Add files via upload

---
+++ 158-project-main/js/highlights.js
@@ -0,0 +1,44 @@
+AFRAME.registerComponent("cursor-listener", {
+  schema: {
+    selectedItemId: { default: "", type: "string" },
+  },
+  init: function () {
+   //call the functions 
+
+  },  
+  handleMouseEnterEvents: function () {
+    // Mouse Enter Events
+    this.el.addEventListener("mouseenter", () => {
+      const id = this.el.getAttribute("id");
+      const postersId = [
+        "superman",
+        "spiderman",
+        "captain-aero",
+        "outer-space",
+      ];
+      if (postersId.includes(id)) {
+        const postersContainer = document.querySelector("#posters-container");
+        postersContainer.setAttribute("cursor-listener", {
+          selectedItemId: id,
+        });
+       //set the color 
+
+       
+      }
+    });
+  },
+  handleMouseLeaveEvents: function () {
+    // Mouse Leave Events
+    this.el.addEventListener("mouseleave", () => {
+      const { selectedItemId } = this.data;
+      if (selectedItemId) {
+        const el = document.querySelector(`#${selectedItemId}`);
+        const id = el.getAttribute("id");
+        if (id == selectedItemId) {
+          el.setAttribute("material", { color: "#fff" });
+        }
+      }
+    });
+  },
+  
+});
